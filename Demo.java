32cc98da1fb51a98c31d66c86d35fe5a105b3489
raju <nagaraju.alapati7@gmail.com>
2019-10-17 17:16:17 +0530

    files updated in master

--- Demo.java
+++ Demo.java
@@ -2,5 +2,10 @@ public class Demo{
 	
 	public static void main(String args[]){
 		System.out.println("welcome java");
+		
+		System.out.println("welcome java");
+		System.out.println("welcome java");
+		System.out.println("welcome java");
+		System.out.println("welcome java");
 	}
 }
--- one.java
+++ one.java
@@ -1,5 +1,8 @@
 public class one{
 public static void main(String args[]){
 System.out.println("demo");
+
+System.out.println("welcome java");
+System.out.println("welcome java");
 }
 }
